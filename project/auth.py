59f345b510e28eb735fc359f11fa4ee0eae3f05a
Kostis Bourlas <kostisbourlas@protonmail.com>
2021-08-01 12:11:17 +0300

    Extracted function into a Class

--- project/auth.py
+++ project/auth.py
@@ -5,43 +5,71 @@ import datetime
 import settings
 
 
-def authorize_credentials():
-    client_creds_b64 = _encode_credentials()
-
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": f"Basic {client_creds_b64.decode()}"
-    }
-    data = {
-        "grant_type": "client_credentials"
-    }
-
-    response = requests.post(settings.TOKEN_URL, headers=headers, data=data)
-
-    return response
-
-
-def get_auth_response(response):
-
-    if is_request_valid(response):
-        now = datetime.datetime.now()
-
-        access_token = response.json()['access_token']
-        expires_in = response.json()['expires_in']
-        expires = now + datetime.timedelta(seconds=expires_in)
-        is_expired = expires < now
-
-
-def is_request_valid(response):
-    return response.status_code in range(200, 299)
-
-
-def _encode_credentials():
+class SpotifyAPI(object):
+    access_token = None
+    access_token_expires = datetime.datetime.now()
+    is_access_token_expired = True
     client_id = settings.CLIENT_ID
     client_secret = settings.CLIENT_SECRET
+    token_url = settings.TOKEN_URL
 
-    client_creds = '{}:{}'.format(client_id, client_secret)
-    client_creds = client_creds.encode()
-    client_creds_b64 = base64.b64encode(client_creds)
 
-    return client_creds_b64
+    def __init__(self, client_id, client_secret, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def _encode_credentials(self):
+        """
+        Return a base64 encoded string
+        """
+
+        if self.client_id is None or self.client_secret is None:
+            raise Exception("You have not provided user's credentials")
+
+        client_creds = '{}:{}'.format(self.client_id, self.client_secret)
+        client_creds = client_creds.encode()
+        client_creds_b64 = base64.b64encode(client_creds)
+
+        return client_creds_b64
+
+    def get_post_params(self):
+        """
+        Returns headers and data required for POST request
+        """
+
+        client_creds_b64 = self._encode_credentials()
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {client_creds_b64.decode()}"
+        }
+        data = {
+            "grant_type": "client_credentials"
+        }
+
+        return headers, data
+
+
+    def authorize_credentials(self):
+        headers, data = self.get_post_params()
+
+        response = requests.post(self.token_url, headers=headers, data=data)
+
+        if not self.is_request_successful(response):
+            return False
+
+        now = datetime.datetime.now()
+
+        self.access_token = response.json()['access_token']
+        expires_in = response.json()['expires_in']
+
+        expires = now + datetime.timedelta(seconds=expires_in)
+        self.access_token_expires = expires
+        self.is_access_token_expired = expires < now
+
+        return True
+
+
+    def is_request_successful(self, response):
+        return response.status_code in range(200, 299)
